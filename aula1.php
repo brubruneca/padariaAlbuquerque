15b37f9ba5b9ecfde789ef0d0c226ba2cea17735
torlee <joaovictortorrestorle@gmail.com>
2023-11-16 21:16:19 -0300

    padoca bruneca
    parei no php

---
+++ aula1.php
@@ -0,0 +1,25 @@
+<?php
+
+$nome = "Bruna Albuquerque";
+
+$cidade = "São Paulo-SP";
+
+$idade = "18 anos";
+
+$hobbie = "Dançar";
+
+$curso = "Programação";
+
+$tv = "Barbie";
+
+$comida = "pipoca";
+
+
+
+echo "Meu nome é $nome e moro em $cidade";
+
+echo " Tenho $idade, gosto de $hobbie e estudo $curso";
+
+echo "No meu tempo livre eu gosto de assistir $tv comendo $comida";
+
+?>
